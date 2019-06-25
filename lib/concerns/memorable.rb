3274965a9d83e176740a0d631d3d9ff93984ee76
Gary Wheeler <gkwheeler40@gmail.com>
2019-06-25 14:22:09 +0000

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,17 @@
+module Memorable
+  module ClassMethods
+  def reset_all
+    self.all.clear
+  end
+
+  def count
+    self.all.count
+  end
+  
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
+end
+end
